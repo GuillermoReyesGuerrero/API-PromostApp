cd45a89818257434e60f7eef24e47efe60a22faf
GuillermoReyesGuerrero <reyesguillermo276@gmail.com>
2019-10-31 16:19:52 -0600

    modifiacion

--- src/routes/peticionespromociones.js
+++ src/routes/peticionespromociones.js
@@ -46,13 +46,8 @@ router.post('/promociones',(req,res,next) => {
     });
 });
 
-<<<<<<< HEAD
-router.put('/promociones/:id',(req,res,next) => {
-    mysqlConnection.query('UPDATE Promociones SET? WHERE idPromocion = '+req.params.id,{
-=======
 router.put('/promociones/:idp',(req,res,next) => {
     mysqlConnection.query('UPDATE Promociones SET? WHERE idPromocion = '+req.params.idp,{
->>>>>>> fe0c3adccd66520ffa0c60c7a23676fad6fdaf55
         //idPromocion : req.body.idPromocion,
         Nombre: req.body.Nombre,
         Descripcion: req.body.Descripcion,
@@ -73,8 +68,8 @@ router.put('/promociones/:idp',(req,res,next) => {
     });
 });
 
-router.delete('/promociones/:id', (req, res) => {
-    mysqlConnection.query('DELETE FROM Promociones WHERE idPromocion = '+req.params.id,(err,result) => {
+router.delete('/promociones/:idp', (req, res) => {
+    mysqlConnection.query('DELETE FROM Promociones WHERE idPromocion = '+req.params.idp,(err,result) => {
         if(!err){
             //res.json(result);
             res.status(201).json({
